e496833fffe98c21ba7c2fa26989b19dada46d43
Seongbin Hong <qls056@gmail.com>
2021-11-25 00:07:25 +0900

    Students data / dummyData 자료 구조 변경

--- frontend/src/components/SeatCell.js
+++ frontend/src/components/SeatCell.js
@@ -6,13 +6,19 @@ function Desks({ leftPerson, rightPerson }) {
             <div className="desk__left">
                 <p className="desk__name">{leftPerson.name}</p>
                 <p className="desk__satisfy">
-                    만족도 <span className="desk__satisfy--score">33</span>
+                    만족도
+                    <span className="desk__satisfy--score">
+                        {leftPerson.rating}
+                    </span>
                 </p>
             </div>
             <div className="desk__right">
                 <p className="desk__name">{rightPerson.name}</p>
                 <p className="desk__satisfy">
-                    만족도 <span className="desk__satisfy--score">33</span>
+                    만족도
+                    <span className="desk__satisfy--score">
+                        {rightPerson.rating}
+                    </span>
                 </p>
             </div>
         </div>
@@ -20,17 +26,18 @@ function Desks({ leftPerson, rightPerson }) {
 }
 
 function SeatCell({ duo }) {
-    const { left } = duo;
-    let right = {
-        id: left.id + 1,
+    const { student1 } = duo;
+    let student2 = {
+        id: student1.id + 1,
         name: "None",
+        rating: 0,
     };
 
-    if (duo.right) {
-        right = duo.right;
+    if (duo.student2) {
+        student2 = duo.student2;
     }
 
-    return <Desks leftPerson={left} rightPerson={right} />;
+    return <Desks leftPerson={student1} rightPerson={student2} />;
 }
 
 export default SeatCell;
--- frontend/src/dummyData.js
+++ frontend/src/dummyData.js
@@ -1,30 +1,30 @@
 const dummyData = [
     {
-        left: { id: 1, name: "홍성빈" },
-        right: { id: 2, name: "정보경" },
+        student1: { id: 1, name: "홍성빈", rating: 11 },
+        student2: { id: 2, name: "정보경", rating: 12 },
     },
     {
-        left: { id: 3, name: "정재민" },
-        right: { id: 4, name: "심인용" },
+        student1: { id: 3, name: "정재민", rating: 21 },
+        student2: { id: 4, name: "심인용", rating: 22 },
     },
     {
-        left: { id: 5, name: "이경민" },
-        right: { id: 6, name: "이새결" },
+        student1: { id: 5, name: "이경민", rating: 31 },
+        student2: { id: 6, name: "이새결", rating: 32 },
     },
     {
-        left: { id: 7, name: "서재은" },
-        right: { id: 8, name: "원준연" },
+        student1: { id: 7, name: "서재은", rating: 41 },
+        student2: { id: 8, name: "원준연", rating: 42 },
     },
     {
-        left: { id: 9, name: "정명언" },
-        right: { id: 10, name: "서경수" },
+        student1: { id: 9, name: "정명언", rating: 51 },
+        student2: { id: 10, name: "서경수", rating: 52 },
     },
     {
-        left: { id: 11, name: "구동용" },
-        right: { id: 12, name: "김영찬" },
+        student1: { id: 11, name: "구동용", rating: 61 },
+        student2: { id: 12, name: "김영찬", rating: 61 },
     },
     {
-        left: { id: 11, name: "채영창" },
+        student1: { id: 11, name: "채영창", rating: 33 },
     },
 ];
 
